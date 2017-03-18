5273703de0d1faa44052fec4874d4746c3d9b1d5
Chris Smith <smith@terminallabs.com>
2017-03-18 16:22:44 -0500

    [master] grammer

--- starter.sh
+++ starter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 procID=`sudo ps -aux | pgrep -f "serial_dpot"`
-if ! ["$procID" -eq "$procID"] 2>/dev/null # Check that proc is and integer. It will be empty of no process was found.
+if ! ["$procID" -eq "$procID"] 2>/dev/null # Check that proc is an integer. It will be empty of no process was found.
 then
     sudo kill -SIGINT $procID
 fi
