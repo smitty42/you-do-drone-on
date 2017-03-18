f53b256583703114d092f486841ebd0ebe3ea0d3
Chris Smith <smith@terminallabs.com>
2017-03-18 01:04:41 +0000

    Trying to clean this up a bit to get it ready to take some input.

--- serial_dpot.py
+++ serial_dpot.py
@@ -5,6 +5,9 @@
 
 import sys
 import time
+import signal
+import argparse
+
 import RPi.GPIO as GPIO
 
 SPI_CS_PIN = 17
@@ -32,32 +35,39 @@ def set_value(value):
 
         GPIO.output(SPI_CLK_PIN, True)
         GPIO.output(SPI_CLK_PIN, False)
-level = 62
-# try:
-#     while True:
-#         print 'level:' + str(level)
-#         set_value(level)
-#         level = int(raw_input('enter level\n'))
-# except KeyboardInterrupt:
-#     set_value(62)
-#     GPIO.cleanup()
 
-delay = 0.03
-try:
+def discrete_value_from_input(level):
     while True:
-#       for level in range(0, 256):
-        for level in range(0, 40):
-            print 'level:' + str(level)
-            set_value(level)
-            time.sleep(delay)
+        print 'level:' + str(level)
+        set_value(level)
+        level = int(raw_input('enter level (1 - 40)\n'))
 
-#       for level in range(255, -1, -1):
-        for level in range(40, -1 , -1):
-            print 'level:' + str(level)
-            set_value(level)
-            time.sleep(delay)
+def fade(level,delay):
+        while True:
+    #       for level in range(0, 256):
+            for level in range(0, 40):
+                print 'level:' + str(level)
+                set_value(level)
+                time.sleep(delay)
 
-except KeyboardInterrupt:
-     set_value(1)
-     GPIO.cleanup()
+    #       for level in range(255, -1, -1):
+            for level in range(40, -1 , -1):
+                print 'level:' + str(level)
+                set_value(level)
+                time.sleep(delay)
 
+def quit_gracefully(*args):
+    ## signal sends us args that we don't care about. We will need to be able to accpet them though, hense *args.
+    set_value(255)
+    GPIO.cleanup()
+    exit(0)
+
+def main(level=62, delay=0.03):
+    signal.signal(signal.SIGINT, quit_gracefully)
+    try:
+        fade(level,delay)
+    except KeyboardInterrupt:
+        quit_gracefully()
+
+if __name__ == "__main__":
+    main()
